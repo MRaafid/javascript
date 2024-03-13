488bd6c51b8514c139993f7486a7df9fb4d9538d
zero <muhammadraafid50290@gmail.com>
2024-03-13 14:13:01 +0530

    13/03/2024

---
+++ Flow Control statements/Repeatition (loop)/for loop/for-of.js
@@ -0,0 +1,11 @@
+function a(x)
+    {
+        for (const value of x) {
+            console.log(value)
+        }
+        console.log("")
+    }
+
+    a('apple')
+    a([1,2,3,4])
+    a(['book','pen','fan'])
